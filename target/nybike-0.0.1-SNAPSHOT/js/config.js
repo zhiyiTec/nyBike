74b0b1c1882092b99e06e2bcb89c98c2a0e9890f
zhiyi <zhiyitec@gmail.com>
2019-09-09 13:57:53 +0800

    修改提交状态

--- target/nybike-0.0.1-SNAPSHOT/js/config.js
+++
@@ -1,6 +0,0 @@
-var achobigSize=new BMap.Size(21,50);
-var iconbigSize=new BMap.Size(41,50);
-var achosmallSize=new BMap.Size(10,25);
-var iconsmallSize=new BMap.Size(20,25);
-var achovsmallSize=new BMap.Size(5,10);
-var iconvsmallSize=new BMap.Size(5,10);
