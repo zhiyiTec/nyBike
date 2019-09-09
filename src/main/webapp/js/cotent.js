4f25165adf5dda3d444bb9eb9af09820efdf541c
NineSun <zhiyitec@gmail.com>
2019-09-09 17:19:47 +0800

    新增站点信息显示

--- src/main/webapp/js/cotent.js
+++ src/main/webapp/js/cotent.js
@@ -1,50 +1,24 @@
-function addBikeWindowInfoContent() {
-
-    var bikeWindowInfoContent = "<div class='mapbox-content'>" +
-        "<div class='mapbox-content-top'>" +
-        "<span class='window_lastUpdate'> 0 ms ago </span>" +
-        "<span class='window_info_button'></span>" +
-        "</div>" +
-        "<div class='mapbox-content-header'>" +
-        "<h1 class='mapbox-content-header-stationName'>info_stationName111</h1>" +
-        "</div>" +
-        "<div class='mapbox-content-detail'>" +
-        "<div class='mapbox-content-detail-bikes-available'>" +
-        "<span class='mapbox-content-detail-bikes-available-val'> 20 </span>" +
-        "<span class='mapbox-content-detail-bikes-available-lbl'>Bikes</span>" +
-        "</div>" +
-        "<div class='mapbox-content-detail-docks-available'>" +
-        "<span class='mapbox-content-detail-docks-available-val'> 6 </span>" +
-        "<span class='mapbox-content-detail-docks-available-lbl'>Docks</span>" +
-        "</div>" +
-        "</div>" +
-        "<div class='mapbox-content-footer'>" +
-        "<span class='mapbox-content-footer-shortName'> Bike station:info_shortName </span>" +
-        "</div>" +
-        "</div>";
-
-
-
+function addBikeWindowInfoContent(obj) {
 
     bikeWindowInfoContent = "<div class='mapbox-content'>" +
         "<div class='mapbox-content-top'>" +
         "<span class='window_lastUpdate'> 0 ms ago </span> <span class='window_info_button'></span>" +
         "</div>" +
         "<div class='mapbox-content-header'>" +
-        "<h1 class='mapbox-content-header-stationName'>info_stationName</h1>" +
+        "<h1 class='mapbox-content-header-stationName'>" + obj.name + "</h1>" +
         "</div>" +
         "<div class='mapbox-content-detail'>" +
         "<div class='mapbox-content-detail-bikes-available'>" +
-        "<span class='mapbox-content-detail-bikes-available-val'> 20 </span>" +
+        "<span class='mapbox-content-detail-bikes-available-val'> " + obj.nba + " </span>" +
         "<span class='mapbox-content-detail-bikes-available-lbl'>Bikes</span>" +
         "</div>" +
         "<div class='mapbox-content-detail-docks-available'>" +
-        "<span class='mapbox-content-detail-docks-available-val'> 6 </span>" +
+        "<span class='mapbox-content-detail-docks-available-val'> " + obj.nda + " </span>" +
         " <span	class='mapbox-content-detail-docks-available-lbl'>Docks</span>:" +
         "</div>" +
         "</div>" +
         "<div class='mapbox-content-footer'>" +
-        "<span class='mapbox-content-footer-shortName'> Bike station:info_shortName </span>" +
+        "<span class='mapbox-content-footer-shortName'> Bike station:" + obj.short_name + " </span>" +
         "</div>" +
         "</div>";
 
